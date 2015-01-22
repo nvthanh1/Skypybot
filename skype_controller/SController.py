cd4ae61d69a23f05453597985efb8abc80ff3cb4
Thanh Nguyen <nvthanh1@tma.com.vn>
2015-01-22 07:00:24 -0400

    Update SController.py

--- skype_controller/SController.py
+++ skype_controller/SController.py
@@ -13,7 +13,7 @@ SKYPE_OBJ.Attach()
 
 # Get all contact from object. This function might not be used in this case
 
-
+def get_file():
     """Function to get file contains list of skype's contact"""
     returndata = {}
     try:
@@ -29,7 +29,7 @@ SKYPE_OBJ.Attach()
     return returndata
 
 
-
+def main_function():
     """Runable function"""
     get_file()
 
