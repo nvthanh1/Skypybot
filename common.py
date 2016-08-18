e9fbd541b3fcb84c6d2de9ba1de53886730a67fa
testuser <testuser@example.com>
2016-08-18 16:02:02 +0700

    Add new changes to th branch

--- common.py
+++ common.py
@@ -2,3 +2,6 @@ import os
 def get_project_path():
     root_dir = os.path.abspath(os.path.dirname(__file__))
     return root_dir
+def get_yourproject_path():
+    root_dir = os.path.abspath(os.path.dirname(__file__))
+    return root_dir
